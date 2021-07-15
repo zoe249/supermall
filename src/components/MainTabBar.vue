5f9b64a3a9ad37c00e656762cfc449497d43edab
unknown <1421759951@qq.com>
2021-07-15 15:28:04 +0800

    初始化项目

---
+++ src/components/MainTabBar.vue
@@ -0,0 +1,41 @@
+<template>
+      <tab-bar>
+      <tab-bar-item path="/Home" activeColor="#D4237A">
+         <img slot="item-icon" src="@/assets/img/tabbar/tab-home.png" alt="">
+         <img slot="item-icon-active" src="@/assets/img/tabbar/tab-home1.png" alt="">
+         <div slot="item-name">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category" activeColor="#D4237A">
+        <img slot="item-icon" src="@/assets/img/tabbar/tab-class.png" alt="">
+        <img slot="item-icon-active" src="@/assets/img/tabbar/tab-class1.png" alt="">
+         <div slot="item-name">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart" activeColor="#D4237A">
+        <img slot="item-icon" src="@/assets/img/tabbar/tab-hold.png" alt="">
+        <img slot="item-icon-active" src="@/assets/img/tabbar/tab-hold1.png" alt="">
+         <div slot="item-name">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile" activeColor="#D4237A">
+        <img slot="item-icon" src="@/assets/img/tabbar/tab-my.png" alt="">
+        <img slot="item-icon-active" src="@/assets/img/tabbar/tab-my1.png" alt="">
+         <div slot="item-name">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+   
+</template>
+
+<script>
+import TabBar from './tabbar/TabBar.vue'
+import TabBarItem from './tabbar/TabBarItem.vue'
+export default {
+components: {
+    TabBar,
+    TabBarItem
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :style="activeStyle" :class="{ active: isActive }"><slot name="item-name"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    path: String,
+    activeColor:{
+        type:String,
+        default:'red'
+    }
+  },
+  data() {
+    return {
+    //   isActive: true,
+    };
+  },
+  computed: {
+    isActive(){
+        // return this.$route.path.indexOf(this.path)!==-1
+        return this.$route.path == this.path
+    },
+    activeStyle(){
+        return this.isActive ? {color:this.activeColor}:{}
+    }
+  },
+  methods: {
+    itemClick() {
+      if (this.$route.path != this.path) {
+        this.$router.replace(this.path);
+        console.log(this.$route.path.indexOf());
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  font-size: 15px;
+}
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  vertical-align: middle;
+  margin-bottom: 2px;
+}
+.active {
+  
+}
+</style>
